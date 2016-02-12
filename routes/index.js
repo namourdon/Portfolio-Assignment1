46a3414c9c09ee459f84efa753445b57d4a890b1
namourdon <nash.amourdon@gmail.com>
2016-02-11 22:46:08 -0500

    created partials

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,12 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Nashia Amourdon' });
 });
-
+/*Get the About me page*/
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About me' });
+});
+/*Get the Contact page*/
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact me' });
+});
 module.exports = router;

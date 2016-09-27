47ecf50aa23fedf51a7576e432dcaa5e2da56388
namourdon <nash.amourdon@gmail.com>
2016-09-26 21:52:53 -0400

    more editing

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -2,3 +2,9 @@
     console.log("App Started..");
     
 })();
+$(document).ready(function(){
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        
+    },3000);
+})
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,18 @@ router.get('/', function(req, res, next) {
 });
 /*Get the About me page*/
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About me' });
+  res.render('about', { title: 'About me' });
 });
 /*Get the Contact page*/
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact me' });
+});
+/*Get the Projects page*/
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Online Projects' });
+});
+/*Get the Services page*/
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
 });
 module.exports = router;
